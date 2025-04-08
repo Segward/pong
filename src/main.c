bcc546f1808b158d9c8963dc491951753353f101
Segward <gustask@stud.ntnu.no>
2025-04-08 21:16:19 +0200

    Removed graphics header, made text rendering with ttf

--- src/main.c
+++ src/main.c
@@ -1,19 +1,79 @@
-#include "graphics.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdio.h>
 
 int main() {
-    SDL_Window* window = createWindow("SDL Window", 800, 600);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("Hello SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
     if (!window) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
         return 1;
     }
 
-    SDL_Renderer* renderer = createRenderer(window);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
-        cleanup(window, NULL);
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
         return 1;
     }
 
-    clearScreen(renderer);
-    drawRectangle(renderer, 100, 100, 200, 150, 255, 0, 0);
+    if (TTF_Init() == -1) {
+        printf("TTF_Init Error: %s\n", TTF_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 40, 40, 40, 255);
+    SDL_RenderClear(renderer);
+
+    TTF_Font *font = TTF_OpenFont("font/rockwell.ttf", 24);
+    if (!font) {
+        printf("TTF_OpenFont Error: %s\n", TTF_GetError());
+        TTF_Quit();
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Color color = { 255, 255, 255, 255 };
+    SDL_Surface *surface = TTF_RenderText_Solid(font, "Hello, SDL2!", color);
+    if (!surface) {
+        printf("TTF_RenderText_Solid Error: %s\n", TTF_GetError());
+        TTF_CloseFont(font);
+        TTF_Quit();
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (!texture) {
+        printf("SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
+        SDL_FreeSurface(surface);
+        TTF_CloseFont(font);
+        TTF_Quit();
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    int textWidth, textHeight;
+    SDL_QueryTexture(texture, NULL, NULL, &textWidth, &textHeight);
+
+    SDL_Rect textRect = { 100, 100, textWidth, textHeight };
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, &textRect);
     SDL_RenderPresent(renderer);
 
     SDL_Event event;
@@ -25,8 +85,10 @@ int main() {
         }
       }
     }
-    
-    cleanup(window, renderer);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
 
