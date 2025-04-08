42c81ef51ff48fee0e41775c9050f476e539731a
Segward <gustask@stud.ntnu.no>
2025-04-08 19:42:38 +0200

    Made readme and simple rect drawing

--- main.c
+++ main.c
@@ -14,6 +14,21 @@ int main() {
         return 1;
     }
 
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    
+    SDL_Rect rect = { 200, 150, 100, 100 };
+    SDL_RendererDrawRect(renderer, &rect);
+    SDL_RenderFillRect(renderer, &rect);
+    SDL_RenderPresent(renderer);
 
     SDL_Event event;
     int quit = 0;
