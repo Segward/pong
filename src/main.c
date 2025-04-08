6b8e31bb148ecb5772f69185b120d2b5ed9d539b
Segward <gustask@stud.ntnu.no>
2025-04-08 20:10:19 +0200

    Made graphics header

--- src/main.c
+++ src/main.c
@@ -1,34 +1,19 @@
-#include <SDL2/SDL.h>
-#include <stdio.h>
+#include "graphics.h"
 
 int main() {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL_Init Error: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Window *window = SDL_CreateWindow("Hello SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window* window = createWindow("SDL Window", 800, 600);
     if (!window) {
-        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-        SDL_Quit();
         return 1;
     }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer* renderer = createRenderer(window);
     if (!renderer) {
-        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+        cleanup(window, NULL);
         return 1;
     }
 
-    SDL_SetRenderDrawColor(renderer, 40, 40, 40, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);   
- 
-    SDL_Rect rect = { 200, 150, 100, 100 };
-    SDL_RenderDrawRect(renderer, &rect);
-    SDL_RenderFillRect(renderer, &rect);
+    clearScreen(renderer);
+    drawRectangle(renderer, 100, 100, 200, 150, 255, 0, 0);
     SDL_RenderPresent(renderer);
 
     SDL_Event event;
@@ -40,10 +25,8 @@ int main() {
         }
       }
     }
-
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
+    
+    cleanup(window, renderer);
     return 0;
 }
 
