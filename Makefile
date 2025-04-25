426c993fdbd89efc906e436059d328a1512c830f
Segward <gustask@stud.ntnu.no>
2025-04-26 01:54:11 +0200

    Started with raylib instead of opengl and sdl

--- Makefile
+++ Makefile
@@ -1,27 +1,6 @@
-CC = gcc
-CFLAGS = -Wall -g -I/opt/homebrew/include
-LDFLAGS = -L/opt/homebrew/lib -lSDL2 -lSDL2_mixer -lGLEW -lGLFW -framework OpenGL -lm 
-
-SRC = src/main.c
-OBJ = src/main.o
-OUT = pong
-
-src/main.o: $(SRC)
-	@$(CC) $(CFLAGS) -c $(SRC) -o $(OBJ)
-
-$(OUT): $(OBJ)
-	@$(CC) $(OBJ) -o $(OUT) $(LDFLAGS)
-
-build: $(OUT)
-	@echo "Built $(OUT)"
-
-run: $(OUT)
-	@echo "Running $(OUT)"
-	@./$(OUT)
-
 clean:
-	@echo "Cleaning up"
-	@rm -f $(OBJ) $(OUT)
+	rm -f pong *.o
 
-.PHONY: clean run build
-
+compile:
+	@echo "Building the project..."
+	gcc -o pong src/main.c -I/opt/homebrew/include -L/opt/homebrew/lib -lraylib -lm -framework OpenGL -framework Cocoa -framework IOKit
--- src/main.c
+++ src/main.c
@@ -1,99 +1,19 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_mixer.h>
+#include <raylib.h>
 #include <stdio.h>
 
-const char *vertexShaderSource = "#version 330 core\n"
-                                 "layout(location = 0) in vec3 aPos;\n"
-                                 "void main() {\n"
-                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                 "}\0";
+int main(int argc, char *argv[]) {
+  InitWindow(800, 600, "raylib");
 
-const char *fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "void main() {\n"
-                                   "   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
-                                   "}\0";
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
 
-int main() {
+    DrawText("Hello, World!", 190, 200, 20, LIGHTGRAY);
+    DrawText("Press ESC to exit", 190, 220, 20, LIGHTGRAY);
 
-    SDL_Init(SDL_INIT_AUDIO);
-    Mix_Init(MIX_INIT_MP3 | MIX_INIT_OGG);
-    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+    EndDrawing();
+  }
 
-    Mix_Music *wander = Mix_LoadMUS("resources/wander.mp3");
-    glfwInit();
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
-    glfwMakeContextCurrent(window);
-    glewInit();
-
-    float vertices[] = {
-        0.5f,  0.5f, 0.0f,   // Top-right corner
-        0.5f, -0.5f, 0.0f,   // Bottom-right corner
-       -0.5f, -0.5f, 0.0f,   // Bottom-left corner
-       -0.5f,  0.5f, 0.0f    // Top-left corner
-    };
-
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-    unsigned int vertexShader, fragmentShader, shaderProgram;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    Mix_PlayMusic(wander, -1);
-
-    while (!glfwWindowShouldClose(window)) {
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(window, 1);
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glfwTerminate();
-
-    return 0;
+  CloseWindow();
+  return 0;
 }
-
